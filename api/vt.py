235650a21c1129c2afea61f2b88d77de13f02adf
cactusjackk7 <cactusjackk7@gmail.com>
2021-02-21 22:11:04 +0100

    working on it later

--- api/vt.py
+++ api/vt.py
@@ -3,4 +3,11 @@
 import json, urllib, urllib2, argparse, hashlib, re, sys
 from pprint import pprint
 
+class vtAPI():
+     def __init__(self):
+         self.api = '<----------PRIVATE-API-KEY-GOES-HERE----->'
+         self.base = 'https://www.virustotal.com/vtapi/v2/'
 
+     def getReport(self,md5):
+          param = {'resource':md5,'apikey':self.api,'allinfo': '1'}
+          url = self
