16a0fe97f7163bced367ffe8b0fe2cc66c4c5142
cactusjackk7 <cactusjackk7@gmail.com>
2021-02-21 23:06:59 +0100

    working on it later

--- api/vt.py
+++ api/vt.py
@@ -10,4 +10,27 @@ class vtAPI():
 
      def getReport(self,md5):
           param = {'resource':md5,'apikey':self.api,'allinfo': '1'}
-          url = self
+          url = self.base + "file/report"
+          data = urllib.urlencode(param)
+          result = urllib2.urlopen(url,data)
+          jdata = json.loads(result.read())
+          return jdata
+
+      def downloadFile(self,md5,name):
+         try:
+            param = {'hash':md5,'apiket':self.api}
+            url = self.base + "file/download"
+            data = urllib.urlencode(param)
+            req = urllib2.Request(url,data)
+            result = urllib2.urlopen(req)
+            downloadfile = result.read()
+            if len(downloadfile) > 0:
+               fo = open(name, 'wb')
+               fo.write(downloadfile)
+               fo.close()
+               print "\n\tMalware Downloaded to File -- " + name
+            else:
+               print md5 + " -- Not Found for Download"
+            except Exception:
+               print md5 + " -- Not Found for Download"
+      def
