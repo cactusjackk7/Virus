067e3e052ce6a46722ea2d22e5950126b64c870c
cactusjackk7 <cactusjackk7@gmail.com>
2021-02-22 17:12:00 +0100

    lite tool for virusses

--- api/vtlite.py
+++ api/vtlite.py
@@ -21,4 +21,38 @@ class vtAPI():
 		data = urllib.urlencode(param)
 		result = urllib2.urlopen(url,data)
 		print "\n\tVirus Total Rescan Initiated for -- " + md5 + " (Requery in 10 Mins)"
+		# Md5 Function
 		
+		def checkMD5(checkval):
+			if re.match(r"([a-fA-F\d]{32})", checkval) == None:
+				md5 = md5sum(checkval)
+				return md5.upper()
+			else: 
+				return checkval.upper()
+			
+		def md5sum(filename):
+			fh = open(filename, 'rb')
+			m = hashlib.md5()
+			while True:
+					data = fh.read(8192)
+					if not data:
+							break
+					m.update(data)
+			return m.hexdigest() 
+		
+		def parse(it, md5, verbose, jsondump):
+			if it['response_code'] == 0:
+				print md5 + " -- Not Found in VT"
+				return 0
+			print "\n\tResults for MD5: ",it['md5'],"\n\n\tDetected by: ",it['positives'],'/',it['total'],'\n'
+			if 'Sophos' in it['scans']:
+				print '\tSophos Detection:',it['scans']['Sophos']['result'],'\n'
+			if 'Kaspersky' in it['scans']:
+				print '\tKaspersky Detection:',it['scans']['Kaspersky']['result'], '\n'
+			if 'ESET-NOD32' in it['scans']:
+				print '\tESET Detection:',it['scans']['ESET-NOD32']['result'],'\n'
+				
+			print '\tScanned on:',it['scan_date']
+			
+			if jsondump == True:
+				jsondumpfile = open("VTDL" + md5 + ".json", "w")
